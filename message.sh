52559592c7fdda2136989e5e550d19bcc57fae7c
osorosia <64416398+osorosia@users.noreply.github.com>
2022-02-05 08:22:51 +0000

    見やすくした

--- message.sh
+++ message.sh
@@ -4,13 +4,16 @@ if [ ! -f ./.test/usleep ]; then
 fi
 
 ./server &>output.tmp &
+
 ps -a | grep server | grep -v grep | grep -v defunct
 PID=$(ps -a | grep server | grep -v grep | grep -v defunct | awk '{print $1}')
 MESSAGE=$1
 ./client $PID $MESSAGE
+
 ./.test/.usleep 100000
 kill $PID
 wait
+
 echo ----------------------------------------------------------
 cat output.tmp
 echo
