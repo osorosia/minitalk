4d58396253822b2a601356f6396f6b66cf202a6e
osorosia <64416398+osorosia@users.noreply.github.com>
2022-02-05 08:22:17 +0000

    sleep->usleepに変更

--- message.sh
+++ message.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+if [ ! -f ./.test/usleep ]; then
+    gcc -o ./.test/usleep ./.test/usleep.c
+fi
+
 ./server &>output.tmp &
 ps -a | grep server | grep -v grep | grep -v defunct
 PID=$(ps -a | grep server | grep -v grep | grep -v defunct | awk '{print $1}')
 MESSAGE=$1
 ./client $PID $MESSAGE
-sleep 1
+./.test/.usleep 100000
 kill $PID
 wait
 echo ----------------------------------------------------------
